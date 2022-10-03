61ab8f8f3742ec28848d3de2ac9576a56548911f
Rajendra Bisoi <90323977+coderRaj07@users.noreply.github.com>
2022-10-03 08:41:19 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,8 @@
 import Whitelist from '../assets/Whitelist.png';
 import CoinMarketCap from '../assets/Coin MarketCap Clone.png';
 import Invoicing from '../assets/Invoicing.png';
+import defiTippingDapp from '../assets/defiTippingDapp.png';
+import uniswapV3PoolFinder from '../assets/uniswapV3PoolFinder.png';
 import CheckContractAddressOrRegularAddress from '../assets/CheckContractAddressOrRegularAddress.png'
 const Projects = () => {
     return (
@@ -11,7 +13,7 @@ const Projects = () => {
             <div className="projects__cards">
 
                 <div className="projects__card">
-                    <h3>Coin MarketCap Clone</h3>
+                    <h3><b>Coin MarketCap Clone</b></h3>
                     <img src={CoinMarketCap} alt="CoinMarketCap Page" />
                     <p><li>To see Top 100 Cryptocurrencies with Live 24 hour Price Change in percentage as well as Volume Change.</li>
                        <li>This site uses Coinmarket Cap API to fetch the real time transaction details from blockchain using querying algorithms and Nextjs to show the details on frontend </li>
@@ -22,7 +24,7 @@ const Projects = () => {
                 </div>
 
                 <div className="projects__card">
-                    <h3>Whitelist DApp</h3>
+                    <h3><b>Make the Addresses Whitelist</b></h3>
                     <img src={Whitelist} alt="Whitelist Landing page" />
                     <p>
                     <li>We want to give early supporters an access to a whitelist for an NFT collection; where Whitelist access should be given to the first 10 users for free.</li>
@@ -34,7 +36,7 @@ const Projects = () => {
                 </div>
 
                 <div className="projects__card">
-                    <h3>Invoice Keeping DApp</h3>
+                    <h3><b>Invoice Keeping DApp</b></h3>
                     <img src={Invoicing} alt="Invoice Storing Landing Page" />
                     <p><li>This Dapp is used to store buyer's invoicing details using buyer's pan ID</li>
                        <li>Where buyer can see his purchase history with dates using his pan ID.</li>
@@ -46,7 +48,7 @@ const Projects = () => {
                 </div>
 
                 <div className="projects__card">
-                    <h3>Coin MarketCap Clone</h3>
+                    <h3><b>Ethereum Address Validator</b></h3>
                     <img src={CheckContractAddressOrRegularAddress} alt="CheckContractAddressOrRegularAddress Page" />
                     <p><li>To Check the provided address is of Contract Address or Regular Address or Invalid Address</li>
                        <li>This site uses ethersjs to connect frontend with backend of solidity which uses a predefined algorithm to verify the provided address </li>
@@ -55,6 +57,33 @@ const Projects = () => {
                     <a href="https://coderraj07.github.io/Check-Address-is-Contract-Address-or-Regular-Address/" target="_blank" className="button">Site</a>
                     <a href="https://github.com/coderRaj07/Check-Address-is-Contract-Address-or-Regular-Address" target="_blank" className="button">Code</a>
                 </div>
+
+
+                <div className="projects__card">
+                    <h3><b>Buy me a coffee Defi Tipping Dapp</b></h3>
+                    <img src={defiTippingDapp} alt="defiTippingDapp Page" />
+                    <p><li>It is a Dapp for Tipping people with ethers for Coffee</li>
+                       <li>We can tip ethers using this dapp to the service provider, whose service we enjoyed</li>
+                       <li>As well as we can see who tipped and what message they want to send to the service provider</li>
+                    </p>
+
+                    <a href="https://defi-tipping-dapp-9dn2advuw-coderraj07.vercel.app/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/coderRaj07/Defi_tipping-Dapp" target="_blank" className="button">Code</a>
+                </div>
+
+                <div className="projects__card">
+                    <h3><b>Uniswap V3 pool finder</b></h3>
+                    <img src={uniswapV3PoolFinder} alt="CheckContractAddressOrRegularAddress Page" />
+                    <p><li>Earlier I had a problem of getting pool Address to perform swap using Uniswap Labs V3 protocol programmatically</li>
+                       <li>So I created a Dapp using which we can get Pool Address of any two pairs of tokens available on Uniswap V3 Protocol</li>
+                       <br/>
+                    </p>
+
+                    <a href="https://uniswap-v3-pool-address-finder.vercel.app/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/coderRaj07/Uniswap_V3_Pool_Address_Finder" target="_blank" className="button">Code</a>
+                </div>
+
+
             </div>
         </section>
     );
