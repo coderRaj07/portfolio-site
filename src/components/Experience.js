eb387a2c2c10f2be84e542a3d5c3f9094f032167
Rajendra Bisoi <90323977+coderRaj07@users.noreply.github.com>
2023-07-12 13:13:54 +0530

    feat_1: Experience updates

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -23,7 +23,18 @@ const Experience = () => {
                        <li>Getting familiar with Chainlink,Tron</li>
                     </ul>
                 </li>
-                    
+                <li>
+                    <i>TraceX Technologies</i> - <strong> Backend Developer Intern </strong>, 2023 (Jan-July)
+                    <br></br>
+                    <br></br>
+                    <ul>
+                    Worked on
+                       <li>Fixing Backend Bugs</li>
+                       <li>Configuring Backend Tasks such as : API Integration, Data Retrieval etc</li>
+                       <li>Writing Backend scripts for Database Manipulation</li>
+                       <li>Engaging with CURD operations in Database with schema design suggestions</li>
+                    </ul>
+            </li>    
             </ul>
         </div>
     );
